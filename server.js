fa47ba0c58a9b75feca889e625ea3d91a3a768d9
rashi2106 <rashibagga21@gmail.com>
2021-04-23 01:08:35 +0530

    heroku launch

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-var PORT=process.env.PORT || 3000;
 
 app.use("/public", express.static(__dirname + "/public"))
 
@@ -8,6 +7,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("The server is up and running!");
 });
