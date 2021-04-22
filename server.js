7a19d1244ca702c1ec93252b53228c2321a89ebf
rashi2106 <rashibagga21@gmail.com>
2021-04-23 00:58:09 +0530

    heroku launch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-var PORT=process.env.PORT||3000;
+var PORT=process.env.PORT || 3000;
 
 app.use("/public", express.static(__dirname + "/public"))
 
